73da778d3ea8aae40f07d3cbc1be30a9301a230c
Fahd Arhalai <fahd.arh@gmail.com>
2020-04-06 22:41:56 +0100

    Securing transmission using a private key

--- receiver/sub.py
+++ receiver/sub.py
@@ -1,14 +1,25 @@
 import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
-import time
+import time, json
 import _thread as thread
+import re
+
+pattern = re.compile(".*[+#].*")
 
 print("-------------------WELCOME-------------------")
 print("In order to receive the file you must have")
 print("the same key the sender used in transmition.")
 print("---------------------------------------------")
 
-# key = input("Enter the private key: ")
+key = ""
+while bool(pattern.match(key)) or key == "":
+	key = input("Enter a non empty private key: ")
+
+
+fileTopic = "/{0}/file".format(key)
+fileinfoTopic = "/{0}/fileinfo".format(key)
+receiverChannelTopic = "/{0}/receiverChannel".format(key)
+senderChannelTopic = "/{0}/senderChannel".format(key)
 
 filename = "nofilename"
 file = None
@@ -19,13 +30,13 @@ sent = False
 def establish_connection(threadName, delay):
 	print("Waiting for sender to connect...")
 	while not cnx:
-		mqttc.publish("/tqudqWQBjH/senderChannel", "0")
+		mqttc.publish(senderChannelTopic, "0")
 		time.sleep(delay)
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe("/tqudqWQBjH/file")
-    client.subscribe("/tqudqWQBjH/filename")
-    client.subscribe("/tqudqWQBjH/receiverChannel")
+    client.subscribe(fileTopic)
+    client.subscribe(fileinfoTopic)
+    client.subscribe(receiverChannelTopic)
     
 
 def on_message(client, userdata, message):
@@ -34,24 +45,26 @@ def on_message(client, userdata, message):
 	global cnx
 	global counter
 
-	if message.topic == "/tqudqWQBjH/receiverChannel":
+	if message.topic == receiverChannelTopic:
 		msg = str(message.payload.decode("utf-8","ignore"))
 		if msg == "0":
 			print("Sender connected.")
 			cnx = True
 		elif msg == "1":
 			print("File received successfully!")
-			publish.single("/tqudqWQBjH/senderChannel", "1", hostname="mqtt.eclipse.org")
+			publish.single(senderChannelTopic, "1", hostname="mqtt.eclipse.org")
 			file.close()
 			exit()
 
-	elif message.topic == "/tqudqWQBjH/filename":
-		print("Filename received.")
-		filename=message.payload
+	elif message.topic == fileinfoTopic:
+		print("File Info:")
+		fileinfo=json.loads(message.payload.decode("utf-8", "ignore"))
+		print(" - Name : {0}\n - Size : {1} KB\n - Number of chunks : {2}".format(fileinfo["name"], fileinfo["size"], fileinfo["chunks"]))
+		filename=fileinfo['name']
 		print("Begin file writing...")
-		file=open(filename,"wb")
+		file=open(filename, "wb")
 
-	elif message.topic == "/tqudqWQBjH/file":
+	elif message.topic == fileTopic:
 		print("\tReceiving chunk ({0})...".format(counter))
 		file.write(message.payload)
 		counter += 1
--- sender/pub.py
+++ sender/pub.py
@@ -1,23 +1,33 @@
 import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
+import os, math, json, re
+
+pattern = re.compile(".*[+#].*")
 
 print("-------------------WELCOME-------------------")
 print("Make sure the file you are about to transmit")
 print("is located in the same directory.")
 print("---------------------------------------------")
 
-# key = input("Enter the private key: ")
+key = ""
+while bool(pattern.match(key)) or key == "":
+	key = input("Enter a non empty private key: ")
+
+fileTopic = "/{0}/file".format(key)
+fileinfoTopic = "/{0}/fileinfo".format(key)
+senderChannelTopic = "/{0}/senderChannel".format(key)
+receiverChannelTopic = "/{0}/receiverChannel".format(key)
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe("/tqudqWQBjH/senderChannel")
+    client.subscribe(senderChannelTopic)
     print("Waiting for the receiver to connect...")
 
 def on_message(client, userdata, message):
-	if message.topic == "/tqudqWQBjH/senderChannel":
+	if message.topic == senderChannelTopic:
 		msg = str(message.payload.decode("utf-8","ignore"))
 		if msg == "0":
 			print("Receiver is connected.")
-			publish.single("/tqudqWQBjH/receiverChannel", "0", hostname="mqtt.eclipse.org")
+			publish.single(receiverChannelTopic, "0", hostname="mqtt.eclipse.org")
 			transmit()
 		elif msg == "1":
 			print("SUCCESS")
@@ -25,13 +35,20 @@ def on_message(client, userdata, message):
 
 def transmit():
 	counter = 1
+	fileinfo = {}
 	filename = input("Enter filename with extension: ")
 	f=open(filename, "rb") 
-	print("File opened!")
+	print("File opened.")
 
-	print("Sending filename...")
-	publish.single("/tqudqWQBjH/filename", filename, hostname="mqtt.eclipse.org")
-	print("Filename sent successfully!")
+	filesize = round(os.stat(filename).st_size / 1024.0, 2)
+	nbrChunks = math.ceil(os.stat(filename).st_size / 10240)
+
+	fileinfo["name"] = filename
+	fileinfo["size"] = filesize
+	fileinfo["chunks"] = nbrChunks
+
+	print("Sending file info...")
+	publish.single(fileinfoTopic, json.dumps(fileinfo), hostname="mqtt.eclipse.org")
 
 	print("Starting file data transmission")
 	fileContent = f.read(10240)
@@ -39,14 +56,14 @@ def transmit():
 		byteArr = bytearray(fileContent)
 
 		print("\tSending chunk ({0})...".format(counter))
-		publish.single("/tqudqWQBjH/file", byteArr, hostname="mqtt.eclipse.org")
+		publish.single(fileTopic, byteArr, hostname="mqtt.eclipse.org")
 		print("\t\tChunk sent")
 
 		fileContent = f.read(10240)
 		counter += 1
 
 	print("File sent successfully!")
-	publish.single("/tqudqWQBjH/receiverChannel", "1", hostname="mqtt.eclipse.org")
+	publish.single(receiverChannelTopic, "1", hostname="mqtt.eclipse.org")
 	print("Waiting for receiver AKN...")
 
 mqttc=mqtt.Client()
